879cbbc8f03a3d6a01aac1a25858161dadc7f50b
Vince Lu <vincentlu@hotmail.ca>
2020-07-05 21:26:46 -0400

    replaced custom image loader with Image.open()

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def load_image(filename):
 # load an image and predict the class
 def run_example(image):
     # load the image
-    img = load_image(image)
+    img = Image.open(input_buffer)
     # load model
     model = load_model('model.h5')
     # predict the class
