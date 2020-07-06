5ce46969e3056e6314c85be7774bd2075c2b27ce
Vince Lu <vincentlu@hotmail.ca>
2020-07-05 22:12:23 -0400

    fixed tabs...

--- app.py
+++ app.py
@@ -14,17 +14,17 @@ input_buffer = st.file_uploader("Upload a file", type=("png", "jpg"))
 
 # load and prepare the image
 def load_image(filename):
-	# load the image 
-	#img = load_img(filename, target_size=(224, 224))
+    # load the image 
+    #img = load_img(filename, target_size=(224, 224))
     img = Image.open(filename)
-	# convert to array
-	img = img_to_array(img)
-	# reshape into a single sample with 3 channels
-	img = img.reshape(1, 224, 224, 3)
-	# center pixel data
-	img = img.astype('float32')
-	img = img - [123.68, 116.779, 103.939]
-	return img
+    # convert to array
+    img = img_to_array(img)
+    # reshape into a single sample with 3 channels
+    img = img.reshape(1, 224, 224, 3)
+    # center pixel data
+    img = img.astype('float32')
+    img = img - [123.68, 116.779, 103.939]
+    return img
 
 # load an image and predict the class
 def run_example(image):
